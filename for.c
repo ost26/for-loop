13457f247e9db1c015a6342c8315ce9f384d421d
oshwin <iamoswintopno@gmail.com>
2020-08-04 07:16:54 +0530

    for loop is fun

---
+++ for.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+
+    int i, j;
+    for (i = 1; i <= 10; i++)
+    {
+        for (j = 0; j <= 5; j++)
+
+        printf("%d\n", j);
+        printf("%d\n", i);
+        int x = i + j;
+        printf("%d\n", x);
+    }
+
+    printf("Now this is some fun):");
+}
+
